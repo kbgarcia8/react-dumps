441276bc212c6f5707416074dc9e11a981c73f90
kbgarcia8 <kbgarcia8@users.noreply.github.com>
2024-10-30 00:20:38 +0800

    dir restructrue

--- cv-creator/src/App.jsx
+++ cv-creator/src/App.jsx
@@ -1,11 +1,20 @@
 import { useState } from "react";
 import "./styles/App.css";
 import { Header } from "./components/Header.jsx";
+import { WebsiteInfo } from "./components/WebsiteInfo.jsx";
 
-export function App() {
+export default function App() {
   return (
     <>
-      <Header />
+      <Header />        
+        <div className="main">
+          <section className="controls-section">
+            <WebsiteInfo />
+          </section>
+          <section className="preview-section">
+            Dummy Content. Preview side is here coming soon
+          </section>
+        </div>
     </>
   );
 }
---
+++ cv-creator/src/components/WebsiteInfo.jsx
@@ -0,0 +1,15 @@
+import "../styles/WebsiteInfo.css";
+
+export function WebsiteInfo() {
+  return (
+    <>
+      <div className="website-info">
+        <p>
+        This is a CV Creator. Provide the necessary
+        details in order to create a template-based CV and
+        save the file when your done    
+        </p>
+      </div>
+    </>
+  );
+}
