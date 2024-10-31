de3d3f667b518b0a146f8aa77e37d98fae172b0d
kbgarcia8 <kbgarcia8@users.noreply.github.com>
2024-10-31 22:22:22 +0800

    initial forms design

--- cv-creator/src/App.jsx
+++ cv-creator/src/App.jsx
@@ -16,7 +16,29 @@ export default function App() {
         </section>
         <section className="edit-section">
           <div className="edit-information">
-            
+            <div className="personal-info">
+              <form action="">
+                <fieldset>
+                  <legend>Personal Information</legend>
+                  <label htmlFor="first-name">First name:</label>
+                  <input type="text" id="first-name" name="first-name"></input>
+                  <label htmlFor="last-name">Last name:</label>
+                  <input type="text" id="last-name" name="last-name"></input>
+                  <label htmlFor="title">Job/Professional Title:</label>
+                  <input type="text" id="title" name="title"></input>
+                  <label htmlFor="email">Email:</label>
+                  <input type="email" id="email" name="email" />
+                  <label htmlFor="contact-number">Contact Number:</label>
+                  <input type="number" id="contact-number" name="contact-number"/>
+                  <label htmlFor="address">Address:</label>
+                  <input type="text" id="address" name="address" />
+                  <label htmlFor="website">Website:</label>
+                  <input type="url" id="website" name="website" />
+                  <label htmlFor="summary">Summary:</label>
+                  <textarea name="sumamary" id="summary"></textarea>
+                </fieldset>
+              </form>
+            </div>
           </div>
         </section>
         <section className="preview-section">
