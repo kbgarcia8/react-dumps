2772cb33cf3f9defbcf0c801b5014503cbe80825
Karl Garcia <156574512+kbgarcia8@users.noreply.github.com>
2024-11-04 15:49:28 +0800

    created input component

--- cv-creator/src/components/Inputs.jsx
+++ cv-creator/src/components/Inputs.jsx
@@ -1,3 +1,40 @@
-//collate all inputs here, make a props that will determine what input will be used
-//Inputs
-//TextArea
+export function Inputs({
+    inputType,
+    id,
+    labelText,
+    placeholderText,
+    type,
+    value,
+    isRequired = true,
+    rows = "4",
+    cols = "30",
+    onChange,
+  }) {
+    const [required, setRequired] = useState({ required: true });
+    return (
+      <>
+        <div className="input-container">
+          <label htmlFor={id}>{labelText}: </label>
+          {inputType === "textarea" ? (
+            <textarea
+              id={id}
+              placeholder={placeholderText}
+              onChange={onChange}
+              value={value}
+              rows={rows}
+              cols={cols}
+            ></textarea>
+          ) : (
+            <input
+              id={id}
+              placeholder={placeholderText}
+              onChange={onChange}
+              value={value}
+              type={type}
+              {...required}
+            />
+          )}
+        </div>
+      </>
+    );
+  }
