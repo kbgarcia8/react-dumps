aaa77e1cc0e743df877dda7ed450520dbf1f3f08
Karl Garcia <156574512+kbgarcia8@users.noreply.github.com>
2024-11-12 14:26:24 +0800

    fixed display of start and end date to blank if date input is still blank

--- cv-creator/src/components/EducationalInformations/EditEducInfo.jsx
+++ cv-creator/src/components/EducationalInformations/EditEducInfo.jsx
@@ -19,92 +19,98 @@ export function EditEducInfo({
     educationEndDate,
   } = props;
 
-  const rawStartDate = new Date(educationStartDate)
-  const formattedStartDate = `${rawStartDate.toLocaleString('default', { month: 'short' })} ${rawStartDate.getFullYear()}`
-  const rawEndDate = new Date(educationEndDate)
-  const formattedEndDate = `${rawEndDate.toLocaleString('default', { month: 'short' })} ${rawEndDate.getFullYear()}`
+  const rawStartDate = new Date(educationStartDate);
+  const formattedStartDate = `${rawStartDate.toLocaleString("default", {
+    month: "short",
+  })} ${rawStartDate.getFullYear()}`;
+  const rawEndDate = new Date(educationEndDate);
+  const formattedEndDate = `${rawEndDate.toLocaleString("default", {
+    month: "short",
+  })} ${rawEndDate.getFullYear()}`;
+  console.log(isNaN(rawStartDate));
   return (
     <>
       <div className="educ-info-entry" id={id}>
         <div className="educ-info-entry-header">
           <span>{universityName}</span>
           <button id="edit-educ-info-btn" onClick={onClickEdit}>
-            <img src="src/assets/edit.svg" alt="edit-entry" data-index={id} />
+            <img src="assets/edit.svg" alt="edit-entry" data-index={id} />
           </button>
         </div>
         <p>
-          {formattedStartDate}-{formattedEndDate}
+          {!isNaN(rawStartDate) ? formattedStartDate : ""}-
+          {!isNaN(rawEndDate) ? formattedEndDate : ""}
         </p>
-      </div>      
-        {isEditing && (
-          <form id={id} onSubmit={EducInfoSaveEdit} className="educ-info-form">
-            <fieldset>
-              <legend></legend>
-              <Inputs
-                id="school-attended"
-                labelText="University/Institute"
-                placeholderText="University Attended"
-                type="text"
-                dataKey="universityName"
-                value={universityName}
-                onChange={handleEducInfoChange}
+      </div>
+      {isEditing && (
+        <form id={id} onSubmit={EducInfoSaveEdit} className="educ-info-form">
+          <fieldset>
+            <legend></legend>
+            <Inputs
+              id="school-attended"
+              labelText="University/Institute"
+              placeholderText="University Attended"
+              type="text"
+              dataKey="universityName"
+              value={universityName}
+              onChange={handleEducInfoChange}
+            />
+            <Inputs
+              id="degree"
+              labelText="Degree/Major"
+              placeholderText="Degree Finished"
+              type="text"
+              dataKey="degreeFinished"
+              value={degreeFinished}
+              onChange={handleEducInfoChange}
+            />
+            <Inputs
+              id="education-start-date"
+              labelText="Start Date"
+              type="month"
+              dataKey="educationStartDate"
+              value={educationStartDate}
+              onChange={handleEducInfoChange}
+            />
+            <Inputs
+              id="education-end-date"
+              labelText="End Date"
+              type="month"
+              dataKey="educationEndDate"
+              value={educationEndDate}
+              onChange={handleEducInfoChange}
+            />
+            <div className="new-form-buttons-space">
+              <Button
+                text="Delete"
+                source="assets/delete.svg"
+                alt="delete-form"
+                className="form-button"
+                id={`delete-educ-info-${id}`}
+                buttonType="button"
+                processClick={EducInfoDeletion}
               />
-              <Inputs
-                id="degree"
-                labelText="Degree/Major"
-                placeholderText="Degree Finished"
-                type="text"
-                dataKey="degreeFinished"
-                value={degreeFinished}
-                onChange={handleEducInfoChange}
+              <Button
+                text="Cancel"
+                source="assets/handle-x.svg"
+                alt="cancel-edit"
+                className="form-button"
+                id={`cancel-educ-info-${id}`}
+                buttonType="button"
+                processClick={EducInfoCancelEdit}
               />
-              <Inputs
-                id="education-start-date"
-                labelText="Start Date"
-                type="month"
-                dataKey="educationStartDate"
-                value={educationStartDate}
-                onChange={handleEducInfoChange}
+              <Button
+                text="Save"
+                source="assets/check-one.svg"
+                alt="save-form"
+                className="form-button"
+                id={`save-educ-info-${id}`}
+                buttonType="submit"
               />
-              <Inputs
-                id="education-end-date"
-                labelText="End Date"
-                type="month"
-                dataKey="educationEndDate"
-                value={educationEndDate}
-                onChange={handleEducInfoChange}
-              />
-              <div className="new-form-buttons-space">
-                <Button
-                  text="Delete"
-                  source="src/assets/delete.svg"
-                  alt="delete-form"
-                  className="form-button"
-                  id={`delete-educ-info-${id}`}
-                  buttonType="button"
-                  processClick={EducInfoDeletion}
-                />
-                <Button
-                  text="Cancel"
-                  source="src/assets/handle-x.svg"
-                  alt="cancel-edit"
-                  className="form-button"
-                  id={`cancel-educ-info-${id}`}
-                  buttonType="button"
-                  processClick={EducInfoCancelEdit}
-                />
-                <Button
-                  text="Save"
-                  source="src/assets/check-one.svg"
-                  alt="save-form"
-                  className="form-button"
-                  id={`save-educ-info-${id}`}
-                  buttonType="submit"
-                />
-              </div>
-            </fieldset>
-          </form>
-        )}      
+            </div>
+          </fieldset>
+        </form>
+      )}
     </>
   );
 }
