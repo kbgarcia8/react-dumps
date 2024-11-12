1ae4edff6eb8fe624bb0bec171de1054ec8f2e85
Karl Garcia <156574512+kbgarcia8@users.noreply.github.com>
2024-11-12 17:26:11 +0800

    update styling of personal info display, need to center infos after job title

--- cv-creator/src/components/PersonalInformations/DisplayPersonalInfo.jsx
+++ cv-creator/src/components/PersonalInformations/DisplayPersonalInfo.jsx
@@ -13,32 +13,36 @@ export function DisplayPersonalInfo({ props }) {
   return (
     <>
       {(firstName !== "" || lastName !== "") && (
-        <p className="personal-detail-entry">
+        <p className="personal-detail-entry" id="full-name">
           {firstName} {lastName}
         </p>
       )}
-      {jobTitle !== "" && <p className="personal-detail-entry">{jobTitle}</p>}
+      {jobTitle !== "" && (
+        <p className="personal-detail-entry" id="job-title">
+          {jobTitle}
+        </p>
+      )}
       {inputEmail !== "" && (
         <p className="personal-detail-entry">
-          <img src="./src/assets/email.svg" alt="email-icon" />
+          <img src="./assets/email.svg" alt="email-icon" />
           <span>{inputEmail}</span>
         </p>
       )}
       {contactNumber !== "" && (
         <p className="personal-detail-entry">
-          <img src="/src/assets/phone.svg" alt="phone-icon" />
+          <img src="/assets/phone.svg" alt="phone-icon" />
           <span>{contactNumber}</span>
         </p>
       )}
       {inputAddress !== "" && (
         <p className="personal-detail-entry">
-          <img src="/src/assets/pin_loc.svg" alt="pin-location-icon" />
+          <img src="/assets/pin_loc.svg" alt="pin-location-icon" />
           <span>{inputAddress}</span>
         </p>
       )}
       {inputWebsite !== "" && (
         <p className="personal-detail-entry">
-          <img src="/src/assets/web-page.svg" alt="webpage-icon" />
+          <img src="/assets/web-page.svg" alt="webpage-icon" />
           <span>{inputWebsite}</span>
         </p>
       )}
