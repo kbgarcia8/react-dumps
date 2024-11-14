a8948d71ec3c717fd540c0005519871d3b8a2b53
Karl Garcia <156574512+kbgarcia8@users.noreply.github.com>
2024-11-14 17:20:03 +0800

    added pre-filled data for work exp and skills

--- cv-creator/src/components/data.js
+++ cv-creator/src/components/data.js
@@ -10,18 +10,57 @@ export const cvData = {
     },
     educationalInformations: [
         {
-          id: 1,
+          id: 1731574604759,
           universityName: "Pamantasan ng Lungsod ng Maynila",
           degreeFinished: "BS Electronics Engineering",
           educationStartDate: "2014-06",
           educationEndDate: "2017-03"
         },
         {
-          id: 2,
+          id: 1731574604760,
           universityName: "Technological Institute of the Philippines",
           degreeFinished: "BS Electronics Engineering",
           educationStartDate: "2017-06",
           educationEndDate: "2020-03"
         }
+    ],
+    workExperiences: [
+        {
+          id: 1731574652267,
+          companyName: "IBM",
+          jobPosition: "Java Developer",
+          experienceStartDate: "2014-06",
+          experienceEndDate: "2017-03",
+          jobTasks: [
+            "Design, implement and maintain Java-based applications that can be high-volume and low-latency",
+            "Propose changes to current Java infrastructure",
+            "Prepare and produce releases of software components"
+          ]
+        },
+        {
+          id: 1731574652268,
+          companyName: "Smart Telecommunications",
+          jobPosition: "Network Engineer",
+          experienceStartDate: "2017-06",
+          experienceEndDate: "2020-03",
+          jobTasks: [
+            "Configure and install various network devices and services (e.g., routers, switches, firewalls, load balancers, VPN, QoS)",
+            "Perform network maintenance and system upgrades including service packs, patches, hot fixes and security configurations",
+            "Monitor performance and ensure system availability and reliability"
+          ]
+        }
+    ],
+    Skills: [
+      "HTML5",
+      "CSS3",
+      "Java",
+      "JavaScript",
+      "React",
+      "SQL",
+      "MongoDB",
+      "NodeJS",
+      "Python",
+      "Perl",
+      "Bash"
     ]
 }
