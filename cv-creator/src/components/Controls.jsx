dd1797c28d060c69f16baa54d1b720d0d7cd7a18
kbgarcia8 <kbgarcia8@users.noreply.github.com>
2024-11-22 00:12:39 +0800

    added info and aesthetic initial toggle, sample fill data and reset and clear functions

--- cv-creator/src/components/Controls.jsx
+++ cv-creator/src/components/Controls.jsx
@@ -1,7 +1,7 @@
 import "../styles/Controls.css"
 import { Button } from "./Button.jsx";
 
-export function Controls() {
+export function Controls({toggleInformationPanel, sampleDataFill, resetAndClearData, downloadPDF, printPDF}) {
   return (
     <>
       <div className="controls-buttons">
@@ -9,32 +9,41 @@ export function Controls() {
           text="Information"
           source="/src/assets/editor.svg"
           alt="edit-information-icon"
+          buttonType="button"
+          processClick={toggleInformationPanel}
+          dataIndex={0}
         />
         <Button 
           text="Aesthetics"
           source="/src/assets/color-filter.svg"
           alt="edit-aesthetic-icon"
+          processClick={toggleInformationPanel}
+          dataIndex={1}
         />
         <div className="divider"></div>
         <Button 
           text="Sample Fill"
           source="/src/assets/fill.svg"
           alt="sample-fill-icon"
+          processClick={sampleDataFill}
         />
         <Button 
           text="Reset"
           source="/src/assets/clear.svg"
           alt="reset-icon"
+          processClick={resetAndClearData}
         />
         <Button 
           text="Download"
           source="/src/assets/download.svg"
           alt="download-icom"
+          processClick={downloadPDF}
         />
         <Button 
           text="Print"
           source="/src/assets/printer.svg"
           alt="print-icon"
+          processClick={printPDF}
         />
       </div>
     </>
