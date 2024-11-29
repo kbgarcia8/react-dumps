3b1e71164712f1595bd977e6b31de7a1dd04d0f5
kbgarcia8 <kbgarcia8@users.noreply.github.com>
2024-11-30 01:18:31 +0800

    API call test

--- memory-card-game/src/App.jsx
+++ memory-card-game/src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
+import './styles/App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [rawData, setRawData] = useState([])
+  const [championMasterList, setChampionMasterList] = useState([])
+  useEffect(() => {
+    async function fetchMainData() {
+      try {
+        const response = await fetch("https://ddragon.leagueoflegends.com/cdn/14.23.1/data/en_US/champion.json");
+        const result = await response.json();
+        console.log(result)
+        setRawData(result.data)
+        setChampionMasterList(Object.keys(result.data))
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+      // finally {
+        
+      //}
+    };
+
+    fetchMainData();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div style={{display:"flex",flexDirection:"row"}}>
+        {championMasterList.map((champion, index) => (
+          <div key={crypto.randomUUID()}>
+            <p style={{color: "black"}}>{champion}</p>
+            <img src={`https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champion}_0.jpg`} alt={`${champion}-splash-art`} />
+          </div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
 export default App
