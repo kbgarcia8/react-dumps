3a48ab053ca3a76eaf0fccfb5e4fe1fbebc83df6
kbgarcia8 <kbgarcia8@users.noreply.github.com>
2024-12-17 23:52:06 +0800

    testing events

--- test-react-app/src/TestEvent.test.jsx
+++ test-react-app/src/TestEvent.test.jsx
@@ -1,21 +1,23 @@
-// App.test.jsx
+// TestEvent.test.jsx
 
 import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import App from "./App";
+import TestEvent from "./TestEvent";
 
-describe("App component", () => {
+describe("TestEvent component", () => {
   it("renders magnificent monkeys", () => {
     // since screen does not have the container property, we'll destructure render to obtain a container for this test
-    const { container } = render(<App />);
+    const { container } = render(<TestEvent />);
     expect(container).toMatchSnapshot();
+    
   });
 
   it("renders radical rhinos after button click", async () => {
     const user = userEvent.setup();
 
-    render(<App />);
+    render(<TestEvent />);
+    screen.debug();
     const button = screen.getByRole("button", { name: "Click Me" });
 
     await user.click(button);
