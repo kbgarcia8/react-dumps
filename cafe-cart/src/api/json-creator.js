228642fb7ff8eb0b462c334366fb3681b59c7ac1
Karl Garcia <156574512+kbgarcia8@users.noreply.github.com>
2025-01-01 17:15:40 +0000

    json creator done need to move to helper

--- cafe-cart/src/api/json-creator.js
+++ cafe-cart/src/api/json-creator.js
@@ -2,22 +2,27 @@ import axios from 'axios'
 import fs from 'fs'
 import rawData from './data/rawData.js'
 
-const JSONCreator = (entries,basename,type,min,max) => {
+const JSONCreator = (entries,basename,type) => {
     const mappedData = entries.map(entry => ({
         [`${basename}Id`]: entry.id,
         [`${basename}Name`]: entry.name,
         [`${basename}Desc`]: entry.description,
         [`${basename}Image`]: entry.image,
         prices: {
-            [type === "drink" ? regular : solo]: 0,
-            [type === "drink" ? large : share]: 0
+            [type === "drink" ? `regular` : `solo`]: type === "drink" ? 120 : 160,
+            [type === "drink" ? `large` : `share`]: type === "drink" ? 140 : 220
         },
         rating: {
-            rate: 0,
-            count: 0
+            rate: Math.floor(Math.random() * (50 - 10) + 10) / 10,
+            count: Math.floor(Math.random() * (200 - 20 + 1)) + 20
         }
     }))
+    //if (fs.existsSync(`../db/${basename}.json`)) fs.unlink(`../db/${basename}.json`)
+    fs.writeFile(`../db/${basename}.json`, JSON.stringify(mappedData, null, 4),err => {
+        if (err) throw err 
+        console.log("Done writing")
+    })
 }
 
 const icedDrinkData = rawData.icedDrinkInitialData
-console.log(icedDrinkData)
+JSONCreator(icedDrinkData,"icedDrink","drink")
