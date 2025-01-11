b80e43702141133dc9c334db1d0c74d37067c688
kbgarcia8 <kbgarcia8@users.noreply.github.com>
2025-01-11 14:44:24 +0800

    Added prop validation for the WebLayout component

--- cafe-cart/src/components/layout/WebLayout/WebLayout.jsx
+++ cafe-cart/src/components/layout/WebLayout/WebLayout.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types"
 import "./WebLayout.styles"
 
 const WebLayout = ({header, main, footer}) => {
@@ -8,4 +9,10 @@ const WebLayout = ({header, main, footer}) => {
             <footer>{footer}</footer>
         </>
     )
+}
+
+WebLayout.propTypes = {
+    header: PropTypes.node.isRequired,
+    main: PropTypes.node.isRequired,
+    footer: PropTypes.node.isRequired
 }
