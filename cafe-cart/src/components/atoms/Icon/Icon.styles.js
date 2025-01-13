f5fc759f8e703b566fe6e458c870adb154c7cb06
Karl Garcia <156574512+kbgarcia8@users.noreply.github.com>
2025-01-13 10:45:47 +0000

    edit icon styles

--- cafe-cart/src/components/atoms/Icon/Icon.styles.js
+++ cafe-cart/src/components/atoms/Icon/Icon.styles.js
@@ -1,1 +1,16 @@
 import styled from 'styled-components';
+
+const IconImage = styled.img`
+    display: flex;
+    place-items: center;
+    border: 2px solid red;
+    width: 100%;
+    height: 100%;
+`
+
+export const Icon = styled.div`
+    display: flex;
+    place-items: center;
+    background-color: transparent;
+    padding: 1.5rem;
+`;
