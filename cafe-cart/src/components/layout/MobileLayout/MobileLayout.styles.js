5d8702ccbfbb84bd1ad58331f99611e51de076c7
kbgarcia8 <kbgarcia8@users.noreply.github.com>
2025-01-14 23:09:08 +0800

    initial mobile layout component

--- cafe-cart/src/components/layout/MobileLayout/MobileLayout.styles.js
+++ cafe-cart/src/components/layout/MobileLayout/MobileLayout.styles.js
@@ -3,34 +3,35 @@ import styled from "styled-components";
 export const MobileLayoutContainer = styled.div`
     display: grid;
     grid-template-rows: auto 1fr auto;
-    overflow: hidden;
+    grid-template-columns: 1fr;
     height: 100vh;
-    border: 2px solid blue;
+    max-width: 100vw;
     gap: 0;
 `;
 
 export const MobileHeader = styled.div`
-    width: 100vw;
-    height: 5vh;
-    position: absolute;
+    max-width: 100vw;
+    height: 7.5vh;
+    position: relative;
     top: 0;
     grid-row: 1/2;
-    border: 2px solid red;
+    border: 5px solid violet;
 `;
 
 export const MobileContent = styled.main`
     width: 100vw;
-    height: calc(100% - 5vh - 5vh);
-    overflow-y: auto;
+    height: calc(100vh - 15.005vh);
+    overflow-y: scroll;
     grid-row: 2/3;
-    border: 2px solid red;
+    border: 5px solid red;
 `;
 
 export const MobileFooter = styled(MobileHeader)`
-    position: absolute;
-    top: 100%;
+    height: 7.5vh;
+    position: relative;
+    bottom: 0;
     grid-row: 3/4;
-    border: 2px solid red;
+    border: 5px solid black;
 `;
 
 export default {MobileLayoutContainer, MobileHeader, MobileContent, MobileFooter};
