32f4a043e6ee1a7b7dee561602909f6d4d25fa5b
Karl Garcia <156574512+kbgarcia8@users.noreply.github.com>
2025-01-17 07:38:29 +0000

    provided reference materials

--- cafe-cart/src/styles/theme.js
+++ cafe-cart/src/styles/theme.js
@@ -1,1 +1,5 @@
 //put light and dark theme here, research how to do this
+/*Implement theme using useContext 
+    https://medium.com/@ankita.pl277/use-usecontext-and-basic-css-to-create-dark-theme-229db41eadf1
+    https://medium.com/@riteshbhagat/implementing-theme-in-react-using-context-api-196149967c9d
+*/
--- cafe-cart/src/styles/variables.js
+++ cafe-cart/src/styles/variables.js
@@ -1,5 +1,7 @@
 //put typescale, styled component css`` and sizing constants object here
-//research on values recommended for these
+/*research on values recommended for these
+    https://medium.com/codyhouse/create-your-design-system-part-1-typography-7c630d9092bd
+*/
 /* Example
 export const spacing = {
     small: '8px',
