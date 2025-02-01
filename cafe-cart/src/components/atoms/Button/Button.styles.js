cc472193279942ddd050acd085046bfd43324620
kbgarcia8 <kbgarcia8@users.noreply.github.com>
2025-02-01 13:43:25 +0800

    fixed center span and image inside button

--- cafe-cart/src/components/atoms/Button/Button.styles.js
+++ cafe-cart/src/components/atoms/Button/Button.styles.js
@@ -2,13 +2,14 @@ import styled from 'styled-components';
 
 export const Button = styled.button`
     display: flex;
-    place-content: center;
+    align-items: center;
+    justify-items: center;
     background-color: #202234;
     color: #FFFFFF;
     border-radius: 2rem;
     padding: 0.25rem 0.75rem;
     margin: 0.125rem;
-    width: 100%;
+    width: 50%;
     cursor: pointer;
     & .button-icon-and-text { //default styling for icon and text inside button
         width: 100%;
