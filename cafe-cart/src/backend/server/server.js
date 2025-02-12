fb70a8bf82be166ec98f6d095b875a5308b1d9c3
kbgarcia8 <kbgarcia8@users.noreply.github.com>
2025-02-12 23:44:26 +0800

    added menu route

--- cafe-cart/src/backend/server/server.js
+++ cafe-cart/src/backend/server/server.js
@@ -11,8 +11,24 @@ const router = jsonServer.router(path.join(__dirname, "../db/db.json")); // Adju
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+
+console.log(router.db)
+
+server.get("/menu", (req, res) => {
+  const db = router.db; 
+  res.json({
+    icedDrink: db.get("icedDrink"),
+    hotDrink: db.get("hotDrink"),
+    cakes: db.get("cakes"),
+    pastries: db.get("pastries"),
+    pasta: db.get("pasta"),
+    mains: db.get("mains"),
+    sides: db.get("sides")
+  });
+});
+
 server.use(jsonServer.rewriter({  
-  "/menu/iced_drink": "/icedDrink",
+  "/menu/iced_drink/": "/icedDrink/",
   "/menu/hot_drink": "/hotDrink",
   "/menu/cakes": "/cakes",
   "/menu/pastries": "/pastries",
