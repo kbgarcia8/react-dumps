b2b45193331f1cd00e3afdec8de8401235eeb298
Karl Garcia <156574512+kbgarcia8@users.noreply.github.com>
2025-02-20 15:44:37 +0000

    comment clean up

--- cafe-cart/src/styles/variables.js
+++ cafe-cart/src/styles/variables.js
@@ -2,21 +2,6 @@
 /*research on values recommended for these
     https://medium.com/codyhouse/create-your-design-system-part-1-typography-7c630d9092bd
 */
-/* Example
-export const spacing = {
-    small: '8px',
-    medium: '16px',
-    large: '24px',
-    xlarge: '32px',
-    xxlarge: '64px',
-};
-To use in styled components:
-import { v } from '../../styles/variables';
-
-const StyledComponent = styled.div`
-    margin: ${v.spacing.medium};
-`;
-*/
 
 export const v = {
     base: {
