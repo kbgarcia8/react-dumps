0f4ddb4d6b73f6cbb0af35429a7f23b0a5fe0765
kbg1513 <kbgarcia1513@gmail.com>
2025-04-05 16:59:05 +0000

    Continuation of useAuth logic and functions

--- cafe-cart/src/components/pages/WebPages/SignUpPage/SignUpPage.jsx
+++ cafe-cart/src/components/pages/WebPages/SignUpPage/SignUpPage.jsx
@@ -1,34 +1,87 @@
 import {React, useState, useEffect, useMemo} from "react";
 import PropTypes from "prop-types";
+import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../../../context/UserAuthContext";
 import * as styled from './SignUpPage.styles'
 
 const SignUpPage =({}) => {
-    const {
-        usernameRef,
-        passwordRef,
-        handleUsernameEmailChange,
-        handlePasswordChange,
-    } = useAuth();
+    
+    const signupUsernameRef = useRef(null);
+    const signUpPasswordRef = useRef(null);
+    const signUpConfirmPasswordRef = useRef(null);
+
+    const [email, setEmail] = useState("");
+    const [error, setError] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmpass, setConfirmpass] = useState("");
+    //To properly trigger debounce UseEffects
+    const [inputValues, setInputValues] = useState({
+        emailorusername: "",
+        password: "",
+        confirmpassword: "",
+    });
+    const { signUp } = useAuth();
+    let navigate = useNavigate();
+
+    const handleUsernameEmailSignUpChange = (e) => {
+        const { input } = e.currentTarget.dataset;
+        signupUsernameRef.current = e.target.value;
+        setInputValues((prevInputValues) => ({...prevInputValues, [input]: signupUsernameRef.current}))
+    };
+    const handlePasswordSignUpChange = (e) => {
+        const { input } = e.currentTarget.dataset;
+        signUpPasswordRef.current = e.target.value;
+        setInputValues((prevInputValues) => ({...prevInputValues, [input]: signUpPasswordRef.current}))
+    };
+    const handleConfirmPasswordChange = (e) => {
+        const { input } = e.currentTarget.dataset;
+        signUpConfirmPasswordRef.current = e.target.value;
+        setInputValues((prevInputValues) => ({...prevInputValues, [input]: signUpConfirmPasswordRef.current}))
+    };
+    
+    // Debounce effect → Only update state if user stops typing for 1000ms
+    useEffect(() => {
+    const timeout = setTimeout(() => {
+        setEmail(signupUsernameRef.current);
+    }, 1000);
+    
+    return () => clearTimeout(timeout);
+    }, [inputValues.emailorusername]);
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setPassword(signUpPasswordRef.current);
+        }, 1000);
+        
+        return () => clearTimeout(timeout);
+    }, [inputValues.password]);
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setConfirmpass(signUpConfirmPasswordRef.current);
+    }, 1000);
+    
+        return () => clearTimeout(timeout);
+    }, [inputValues.confirmpassword]);
 
     const signUpPageInputHeaders = [
         {
             label: "Email or Username",
             type: "text",
-            refType: "username",
-            handlechange: handleUsernameEmailChange
+            refType: signupUsernameRef,
+            handlechange: handleUsernameEmailSignUpChange
         }, 
         {
             label: "Password",
             type: "password",
-            refType: "password",
-            handlechange: handlePasswordChange
+            refType: signUpPasswordRef,
+            handlechange: handlePasswordSignUpChange
         },
         {
             label: "Confirm Password",
             type: "password",
-            refType: "password",
-            handlechange: handlePasswordChange
+            refType: signUpConfirmPasswordRef,
+            handlechange: handleConfirmPasswordChange
         },
     ];
 
@@ -41,11 +94,26 @@ const SignUpPage =({}) => {
             mainOnChange: signUpInput.handlechange, //use useRef in signup then use useState in sign up
             type: signUpInput.type,
             isRequired: true,
-            ref: signUpInput.refType === "username" ? usernameRef : passwordRef,
+            ref: signUpInput.refType,
             dataAttributes: {
-                "data-input": `${signUpInput.label}`
+                "data-input": `${signUpInput.label.toLowerCase().replace(/\s+/g, '')}`
             }
     }))
+
+    const handleSignupSubmit = async (e) => {
+        e.preventDefault();
+        setError("");
+        try {
+            if(password === confirmpass) {
+                await signUp(email, password);
+                navigate("/");
+            } else if (password !== confirmpass) {
+                setError("Password does not match!")
+            }
+        } catch (err) {
+            setError(err.message);
+        }
+    };
 
     return(
         <styled.SignUpPageWrapper>
@@ -59,6 +127,7 @@ const SignUpPage =({}) => {
                 labelClassName={"signup-inputs-label"}
                 hasSubmit
                 submitText={"Confirm"}
+                handleSubmit={handleSignupSubmit}
                 hasCancel
                 cancelText={"Login"}
             />
